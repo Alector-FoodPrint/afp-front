6dbfb915291b994df02ea73c567b69d3dbd1e9af
alector <panagiotispp@gmail.com>
2021-08-28 20:04:16 +0300

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.css"
+import Head from "next/head"
+// import "tailwindcss/tailwind.css"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Alector FoodPrint</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
