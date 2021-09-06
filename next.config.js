c49912b064d0c962457fa0b9122bfc6418246683
alector <panagiotispp@gmail.com>
2021-09-06 16:29:14 +0300

    next configuration

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   reactStrictMode: true,
+  eslint: {
+    dirs: ["components", "context", "contracts", "data", "hooks", "pages", "utils"] // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+  }
 }
