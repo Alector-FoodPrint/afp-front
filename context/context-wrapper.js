9eecaadefc0c0355fdd35ebb09b82f15fda4c1a3
alector <panagiotispp@gmail.com>
2021-09-06 16:29:48 +0300

    context wrapper

--- context/context-wrapper.js
+++ context/context-wrapper.js
@@ -11,6 +11,26 @@ import { FOOD_TYPES } from "/data/food-types"
 export const contextAfp = React.createContext(null)
 export const contextData = React.createContext(null)
 
+const userFilter = function (obj, predicate) {
+  let result = [],
+    key
+
+  for (key in obj) {
+    // console.log({ key })
+    // console.log("object:", obj[key])
+
+    if (obj.hasOwnProperty(key) && predicate(obj[key])) {
+      console.log("key", key)
+
+      console.log("object", obj[key])
+      result.push(obj[key])
+    }
+  }
+
+  console.log(result)
+  return result
+}
+
 const ContextWrapper = props => {
   const myNFT = useContract(AfpAddress, AfpAbi)
 
@@ -27,6 +47,9 @@ const ContextWrapper = props => {
       result.USERS = USERS
       result.SOLD = SOLD
       result.FOOD_TYPES = FOOD_TYPES
+      result.USERS.VENDORS = userFilter(USERS, item => item["role"] == "vendor")
+      result.USERS.PRODUCERS = userFilter(USERS, item => item["role"] == "producer")
+      result.USERS.WAREHOUSES = userFilter(USERS, item => item["role"] == "warehouse")
 
       setGlobalData(result)
     }
